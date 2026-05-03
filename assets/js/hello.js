6e938ed3afa139c739c7892a0c39cdbaaf1e5073
minyoung-05 <cherrypie0517@snu.ac.kr>
2026-05-03 14:39:10 +0000

    JavaScript 연결

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,12 +1,9 @@
 // assets/js/hello.js
-const author = "이상";
-const title = "날개";
-const year = 1936;
-const today = 2026;
-const yearsPassed = today - year;
+const author = "김민영";
+const major = "영어영문학과 전공";
+const year = 2026;
 
 console.log(
-    `안녕하세요! ${author}의 「${title}」 (${year}) ` +
-    `페이지에 오신 것을 환영합니다.`
+    `안녕하세요! ${author}(${major})의 사이트입니다.` +
+    `이 사이트는 ${year}에 개설되었습니다.`
 );
-console.log(`이 작품은 발표된 지 ${yearsPassed}년이 되었습니다.`);
