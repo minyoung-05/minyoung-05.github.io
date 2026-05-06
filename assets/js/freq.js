44a8e888dcb9925d1324b9724109a09740f3ad48
minyoung-05 <cherrypie0517@snu.ac.kr>
2026-05-06 07:54:40 +0000

    변수명 오타 수정

--- assets/js/freq.js
+++ assets/js/freq.js
@@ -25,4 +25,4 @@ for (let i = 1; i < counts.length; i++) {
     if (counts[i] > counts[maxIdx]) maxIdx = i;
 }
 const topTarget = targets[maxIdx];
-console.log(`가장 자주 나온 글자: '${top}' (${counts[maxIdx]}번)`);
+console.log(`가장 자주 나온 글자: '${topTarget}' (${counts[maxIdx]}번)`);
